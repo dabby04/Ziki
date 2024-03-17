29e2c3aaa894e1ab061e62a6b2d6d38b1f0bd3f9
dabby04 <97193496+dabby04@users.noreply.github.com>
2024-03-17 16:19:44 -0700

    Added field to users

---
+++ ddl/discussion.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users{
+    userId          INT IDENTITY
+    firstName       VARCHAR(40),
+    lastName        VARCHAR(40),
+    email           VARCHAR(50),
+    dob             DATE,
+    username        VARCHAR(50),
+    password        VARCHAR(30),
+    profile_photo    VARBINARY(MAX)
+}
+
+CREATE TABLE 
