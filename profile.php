ebe0337e2be3fe8139427f5aceeef47a6a47c85e
abstractafua <113399971+abstractafua@users.noreply.github.com>
2024-03-24 11:10:08 -0700

    profile picture visible

--- profile.php
+++ profile.php
@@ -10,24 +10,12 @@
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
     <script src="script/profile.js"></script>
 </head>
+
+
+
+
 <header class="header">
     <h1 class="title">Ziki</h1>
-    <img id="header-img" src="images/headerimg.png" />
-    <img class="pfp" src="images/blank-profile-picture.png"/>
-    <div id="postPopup" class="popup">
-        <textarea placeholder="Speak your mind..."> </textarea>
-        <div class="inputControls">
-            <img id="photo_icon" src="images/photo_icon.png"/>
-            <button class="postButtons" onclick="submitPost()">Submit</button>
-            <button class="postButtons" id="XButton" onclick="closePostPopup()">X</button>
-        </div>
-    </div>
-</header>
-<body>
-    <div class="profile-info">
- 
-    </div>
-
     <?php
     error_reporting(E_ALL); 
     ini_set('display_errors', 1);
@@ -44,6 +32,7 @@
         $user_id = '1';
     }
 
+
     // Query to fetch user information from the database
     $sql = "SELECT * FROM users WHERE id = $user_id";
     $result = mysqli_query($conn, $sql);
@@ -53,7 +42,30 @@
         // Fetch the user's data
         $userData = mysqli_fetch_assoc($result);
         ?>
+    
+
+    <img id="header-img" src="images/headerimg.png" />
+    <?php if (!empty($userData['pfp'])): ?>
+            <img class="pfp" src="data:image/jpeg;base64,<?php echo base64_encode($userData['pfp']); ?>"/> 
+            <?php else: ?>
+            <img class="pfp" src="images/blank-profile-picture.png"/>
+        <?php endif; ?>
+
+    <div id="postPopup" class="popup">
+        <textarea placeholder="Speak your mind..."> </textarea>
+        <div class="inputControls">
+            <img id="photo_icon" src="images/photo_icon.png"/>
+            <button class="postButtons" onclick="submitPost()">Submit</button>
+            <button class="postButtons" id="XButton" onclick="closePostPopup()">X</button>
+        </div>
+    </div>
+</header>
+<body>
+  
+ 
+
         <div class="profile-info">
+
             <p id="username">@<?php echo $userData['username']; ?></p>
             <?php
             // Calculate and display age based on date of birth (DOB)
@@ -81,12 +93,16 @@ if(isset($_GET['id'])) {
 }
 ?>
 
-        
         <?php
     } else {
         echo 'Error fetching user information';
     }
     ?>
+
+
+
+
+
     <nav id="tab-tool">
         <div class="text-option" onclick="toggle()"><a href="">Posts</a></div> 
         <div class="text-option" id="last" onclick="toggle()"><a href=""> Discussions</a></div>
