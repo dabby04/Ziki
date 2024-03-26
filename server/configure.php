5241a6f480f57a1d5d851454545e1dc0e92538d0
dabby04 <97193496+dabby04@users.noreply.github.com>
2024-03-25 23:15:41 -0700

    Updated the post management page

--- server/configure.php
+++ server/configure.php
@@ -1,32 +1,32 @@
 <?php
-// require "conn_info.php";
-// try {
-//     $connString = "mysql:host=localhost;dbname=ziki";
-//     $user = DBUSER;
-//     $pass = DBPASS;
-//     $pdo = new PDO($connString, $user, $pass);
-//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//     // print_r("connected");
+require "conn_info.php";
+try {
+    $connString = "mysql:host=localhost;dbname=ziki";
+    $user = DBUSER;
+    $pass = DBPASS;
+    $pdo = new PDO($connString, $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // print_r("connected");
 
-// } catch (PDOException $e) {
-//     die ($e->getMessage());
-// }
-// // 
-
- $DBHOST = '127.0.0.1';
- $DBPORT = '3307'; // Change this to your MySQL port if it's different
- $DBNAME = 'Ziki_DB';
- $DBUSER = 'root';
- $DBPASS = '';
-
- try {
-     $dsn = "mysql:host=$DBHOST;port=$DBPORT;dbname=$DBNAME;charset=utf8mb4";
-     $options = [
-         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_EMULATE_PREPARES => false,     ];
-     $pdo = new PDO($dsn, $DBUSER, $DBPASS, $options);
 } catch (PDOException $e) {
-     die("Connection failed: " . $e->getMessage());
- }
+    die ($e->getMessage());
+}
+// 
+
+//  $DBHOST = '127.0.0.1';
+//  $DBPORT = '3307'; // Change this to your MySQL port if it's different
+//  $DBNAME = 'Ziki_DB';
+//  $DBUSER = 'root';
+//  $DBPASS = '';
+
+//  try {
+//      $dsn = "mysql:host=$DBHOST;port=$DBPORT;dbname=$DBNAME;charset=utf8mb4";
+//      $options = [
+//          PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+//         PDO::ATTR_EMULATE_PREPARES => false,     ];
+//      $pdo = new PDO($dsn, $DBUSER, $DBPASS, $options);
+// } catch (PDOException $e) {
+//      die("Connection failed: " . $e->getMessage());
+//  }
  ?>
 
