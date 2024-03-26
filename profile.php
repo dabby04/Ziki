4d0e7cce7a2dcfd1308068af9adbc5886d2c0135
abstractafua <113399971+abstractafua@users.noreply.github.com>
2024-03-26 11:58:18 -0700

    profile post css fixes

--- profile.php
+++ profile.php
@@ -1,3 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+    <link rel="stylesheet" href="css/pageheader.css" />
+    <link rel="stylesheet" href="css/profile.css?v=<?php echo time(); ?>">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+    <script src="script/profile.js"></script>
+</head>
+
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
@@ -32,28 +47,16 @@ try {
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profile</title>
-    <link rel="stylesheet" href="css/pageheader.css" />
-    <link rel="stylesheet" href="css/profile.css" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
-    <script src="script/profile.js"></script>
-</head>
 
 <header class="header">
     <h1 class="title"><a href="home.php">Ziki</a></h1>
     <?php if (!empty($userData['profilePhoto'])): ?>
         <img id="header-img" src="images/headerimg.png" />
-        <img class="pfp" src="data:image/jpeg;base64,<?php echo base64_encode($userData['profilePhoto']); ?>" style="border-radius: 50%; width: 200px; position: absolute; top: 60%; left: 50%; transform: translate(-50%, -50%);" />
+        <img class="pfp" src="data:image/jpeg;base64,<?php echo base64_encode($userData['profilePhoto']); ?>"  />
     <?php else: ?>
         <img id="header-img" src="images/headerimg.png" />
-        <img class="pfp" src="images/blank-profile-picture.png" style="border-radius: 50%; width: 200px; position: absolute; top: 60%; left: 50%; transform: translate(-50%, -50%);" />
+        <img class="pfp" src="images/blank-profile-picture.png"  />
     <?php endif; ?>
     
     <!-- Post Popup Form -->
@@ -61,8 +64,8 @@ try {
         <form id="postForm" action="savePost.php" method="post" enctype="multipart/form-data">
             <textarea name="content" placeholder="Speak your mind..."></textarea>
             <div class="inputControls">
-                <input type="text" name="title" placeholder="Enter title" is="postTitle" style="display: flex; position: absolute; top: -825%; width: 30em; background-color: #d9d9d9; left: -4%; border-radius: 8px;">
-                <button type="button" class="postButtons" id="XButton" onclick="closePostPopup()" style="border: none; background-color: #d9d9d9; width: 20pt; position: absolute; bottom: 720%; right: -2%; font-size: 18px; cursor: pointer; margin-right: 20px;">X</button>
+                <input type="text" name="title" placeholder="Enter title" id="postTitle">
+                <button type="button" class="postButtons" id="XButton" onclick="closePostPopup()">X</button>
                 <label for="postImage">
                     <img id="photo_icon" src="images/photo_icon.png" alt="Photo Icon"/>
                 </label>
