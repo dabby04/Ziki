aedf135521191cf1d0e2342abfc719b606818cf2
dabby04 <97193496+dabby04@users.noreply.github.com>
2024-03-27 09:42:06 -0700

    Testing connection info for server

--- server/conn_info.php
+++ server/conn_info.php
@@ -1,8 +1,8 @@
 <?php
 
 
-define('DBUSER', 'webuser');
-define('DBPASS', 'Password$$');
+define('DBUSER', '84518448');
+define('DBPASS', '84518448');
 
 // define('DBHOST', '127.0.0.1');
 // define('DBNAME', 'Ziki_DB');
