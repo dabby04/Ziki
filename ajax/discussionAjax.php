e5e1120de2eaa456874eca315e27b9ca435108da
Peter Okpoga Idoko <idokopeterokpoga@gmail.com>
2024-04-10 23:01:54 -0700

    discussion page ajax done and working

--- ajax/discussionAjax.php
+++ ajax/discussionAjax.php
@@ -48,7 +48,7 @@ $jsArray = json_encode($list); // Initialize as an empty JSON array
       } else {
         $theme = $_GET["theme"];
         $formattedtheme = strtolower($theme);
-        print_r($formattedtheme);
+        //print_r($formattedtheme);
         $search_query = $formattedtheme; // Add wildcards here
         $sql = "SELECT * FROM POSTS WHERE theme = ?";
       }
@@ -59,8 +59,43 @@ $jsArray = json_encode($list); // Initialize as an empty JSON array
 
       header('Content-Type: application/json');
       if ($statement->rowCount() > 0) {
-        $list = $statement->fetchAll(PDO::FETCH_ASSOC); // Fetch all rows
-        echo json_encode($list);
+        // $list = $statement->fetchAll(PDO::FETCH_ASSOC); // Fetch all rows
+        // echo json_encode($list);
+        while($row= $statement->fetch(PDO::FETCH_ASSOC)) {
+          echo "<div class='card text-center'>
+                                        <div class='card-header'>
+                                          <img id='discussionPFP' class='icons' src='images/blank-profile-picture.png' alt ='disussion pfp'>".$row["creator"]. 
+                                          "<div class='dropdown'>
+                                          <img id='discDropdown' class='icons' src='images/dropdown.png' alt='dropdown discussion' data-bs-toggle='dropdown' aria-expanded='false'>
+                                          <ul class='dropdown-menu'>
+                                            <li><a class='dropdown-item' href='#'>Flag</a></li>
+                                            <li><a class='dropdown-item' href='#'>Save</a></li>
+                                            <li><a class='dropdown-item' href='#'>Share</a></li>
+                                          </ul>
+                                          </div>
+                                          <img id='discFav' src='images/star.png' alt='favorite discussion'onClick={addFav(".$row["creatorId"].",".$row["id"].")}>
+                                         
+                                        </div>
+                                          <div class='card-body'>
+                                              <h5 class='card-title'>".$row["title"]."</h5>
+                                              <p class='card-text'>".$row["content"]."</p>
+                                              <form action='specificDiscussion.php' method='GET'>
+                                                        <button type='submit' class='btn btn-primary' name='discTopic' value=".$row["id"].">View Discussion</button>
+                                                         </form>
+                                          </div>
+                                          <div class='card-footer text-body-secondary'>"
+                                              .$row["created_at"]."
+
+                                              <div class= 'footIcons'>".
+                                                $row["likes"]."
+                                                <img src='images/like-icon-on-transparent-background-free-png.png' alt= 'like' id='like'>
+                                                ".
+                                                $row["dislikes"]."
+                                                <img src='images/like-icon-on-transparent-background-free-png.png' alt= 'dislike' id='dislike'>
+                                              </div>
+                                          </div>
+                                      </div>";
+        }
       } else {
         $message = "No posts found";
         echo "<script type='text/javascript'>alert('$message');</script>";
--- script/discussion.js
+++ script/discussion.js
@@ -1,71 +1,22 @@
-function populate(type,value){
-    //window.onload = function () {
-        var xhttp = new XMLHttpRequest();
-        var topics;
-        xhttp.onreadystatechange = function() {
-          if (this.readyState == 4 && this.status == 200) {
-          //console.log(type + value)
-                topics = JSON.parse(xhttp.responseText);
-              console.log(topics);
-            }
-        };
-          // Specify the HTTP method (GET or POST) and the PHP file to call
-        xhttp.open("GET", "ajax/discussionAjax.php?"+type+"="+value,true);
-          console.log("here");
-          // Send the request
-        xhttp.send();
-        console.log("done");
-  
-          console.log(topics+"yeahhh");
-          //using the name of the discussion, generate related content
-          const displayComments = document.getElementById("cards");
-          displayComments.innerHTML = topics.map((e) => {
-            return `<div class="card text-center">
-                              <div class="card-header">
-                                <img id="discussionPFP" class="icons" src="images/blank-profile-picture.png" alt ="disussion pfp">
-                                ${e.creator} 
-                                <div class="dropdown">
-                                <img id="discDropdown" class="icons" src="images/dropdown.png" alt="dropdown discussion" data-bs-toggle="dropdown" aria-expanded="false">
-                                <ul class="dropdown-menu">
-                                  <li><a class="dropdown-item" href="#">Flag</a></li>
-                                  <li><a class="dropdown-item" href="#">Save</a></li>
-                                  <li><a class="dropdown-item" href="#">Share</a></li>
-                                </ul>
-                                </div>
-                                <img class="discFav" src="images/star.png" alt="favorite discussion" onClick={addFavorite}>
-                               
-                              </div>
-                                <div class="card-body">
-                                    <h5 class="card-title">${e.title}</h5>
-                                    <p class="card-text">${e.content}</p>
-                                    <form action="specificDiscussion.php" method="GET">
-                                              <button type="submit" class="btn btn-primary" name="discTopic" value=${e.id}>View Discussion</button>
-                                               </form>
-                                </div>
-                                <div class="card-footer text-body-secondary">
-                                    ${e.created_at}
-    
-                                    <div class= "footIcons">
-                                      ${e.likes}
-                                      <img src="images/like-icon-on-transparent-background-free-png.png" alt= "like" id="dislike">
-                                      </div>
-                                      <div class= "footicons">
-                                       ${e.dislikes}
-                                      <img src="images/like-icon-on-transparent-background-free-png.png" alt= "like" id="like">
-                                    </div>
-                                </div>
-                            </div>`;
-          }).join("");
+
         
         var fav = 0;
-        $(document).on('click', '.discFav', function() {
-          
+        //$(document).on('click', '#discFav', function() {
+        function addFav(user, post){
           if(fav==0)
           {
             $(this).css({
               'background-color': 'Yellow'
             });
             fav =1;
+            // var xmlhttp = new XMLHttpRequest();
+            //   xmlhttp.onreadystatechange = function() {
+            //     if (this.readyState == 4 && this.status == 200) {
+            //       alert("Post successfully added to favorites")
+            //     }
+            //   };
+            //   xmlhttp.open("POST", "./php/addFavorite.php?user="+user+"&fav="+post, true);
+            //   xmlhttp.send();
           }
           else{
             $(this).css({
@@ -74,7 +25,7 @@ function populate(type,value){
             fav =0;
           }
     
-        });
+        }//);
     
     
         var like = 0;
@@ -96,7 +47,6 @@ function populate(type,value){
     
     var dislike = 0;
     $(document).on('click', '#dislike', function() {
-      // Add your code here for the #dislike click event
       if(dislike==0)
       {
         $(this).css({
@@ -112,51 +62,57 @@ function populate(type,value){
       }
     });
     
-    
-        
-    }
-        function addFavorite(id) {
-          // Create an XMLHttpRequest object
-          var xhttp = new XMLHttpRequest();
-  
-          // Specify the HTTP method (GET or POST) and the PHP file to call
-          xhttp.open("GET", "ajax/discussionAjax.php?function=addFavorites("+id+")", true);
-  
-          // Send the request
-          xhttp.send();
-        //}
-      }
 
-      function generate(topics)
+
+function discussion(type,value)
+{
+  
+  var content = "";
+  const xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if(this.readyState==4 && this.status == 200)
       {
-        const displayComments = document.getElementById("cards");
-        
-        displayComments.innerHTML = topics.map((e) => {
-          return `<div class="card text-center">
-                            <div class="card-header">
-                              <img id="discussionPFP" class="icons" src="images/blank-profile-picture.png" alt ="disussion pfp">
-                              ${e.creator} 
-                              <div class="dropdown">
-                              <img id="discDropdown" class="icons" src="images/dropdown.png" alt="dropdown discussion" data-bs-toggle="dropdown" aria-expanded="false">
-                              <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Flag</a></li>
-                                <li><a class="dropdown-item" href="#">Save</a></li>
-                                <li><a class="dropdown-item" href="#">Share</a></li>
-                              </ul>
-                              </div>
-                              <img id="discFav" src="images/star.png" alt="favorite discussion">
-                             
-                            </div>
-                              <div class="card-body">
-                                  <h5 class="card-title">${e.title}</h5>
-                                  <p class="card-text">${e.content}</p>
-                                  <form action="specificDiscussion.php" method="GET">
-                                            <button type="submit" class="btn btn-primary" name="discTopic" value=${e.id}>View Discussion</button>
-                                             </form>
-                              </div>
-                              <div class="card-footer text-body-secondary">
-                                  ${e.created_at}
-                              </div>
-                          </div>`;
-        }).join("");
+        content = this.responseText;
+        generate(content);
       }
+    }
+    xmlhttp.open("GET", "./ajax/discussionAjax.php?"+type+"="+value,true);
+  xmlhttp.send();
+}
+
+function generate(content)
+{
+  var topics = content;
+  console.log(topics);
+  var cards = document.getElementById("cards");
+  cards.innerHTML = topics;
+  // .map((e) => {
+  //             return `<div class="card text-center">
+  //                               <div class="card-header">
+  //                                 <img id="discussionPFP" class="icons" src="images/blank-profile-picture.png" alt ="disussion pfp">
+  //                                 ${e.creator} 
+  //                                 <div class="dropdown">
+  //                                 <img id="discDropdown" class="icons" src="images/dropdown.png" alt="dropdown discussion" data-bs-toggle="dropdown" aria-expanded="false">
+  //                                 <ul class="dropdown-menu">
+  //                                   <li><a class="dropdown-item" href="#">Flag</a></li>
+  //                                   <li><a class="dropdown-item" href="#">Save</a></li>
+  //                                   <li><a class="dropdown-item" href="#">Share</a></li>
+  //                                 </ul>
+  //                                 </div>
+  //                                 <img id="discFav" src="images/star.png" alt="favorite discussion">
+                                 
+  //                               </div>
+  //                                 <div class="card-body">
+  //                                     <h5 class="card-title">${e.title}</h5>
+  //                                     <p class="card-text">${e.content}</p>
+  //                                     <form action="specificDiscussion.php" method="GET">
+  //                                               <button type="submit" class="btn btn-primary" name="discTopic" value=${e.id}>View Discussion</button>
+  //                                                </form>
+  //                                 </div>
+  //                                 <div class="card-footer text-body-secondary">
+  //                                     ${e.created_at}
+  //                                 </div>
+  //                             </div>`;
+  //           }).join("");
+
+}
